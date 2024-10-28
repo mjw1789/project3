7ca826598174c6a690662105613b0dd6f293b923
Madelyn Winfrey <mwinfr@gmail.com>
2024-10-28 17:34:03 -0400

    added comments for insert method

--- Node.java
+++ Node.java
@@ -15,15 +15,23 @@ public Node() {
 
 //insert method
   public void insert (Node node, String sequence, int i) {
+
+    //set the current letter to the correct one in the sequence
     if (i < sequence.length()){
       char letter = sequence.charAt(i);
+
+        //if the node is not a leaf then do not create a new node and use the exsiting one
         if (node.isLeaf == false) {
           insert(node.children[letter], sequence, i + 1);
         }
+
+        //if the node is a leaf then create a new node to put the current character in
         if (node.isLeaf == true) {
           node.children[letter] = new Node();
           insert(node.children[letter], sequence, i + 1);
         }
+
+        //if the node is null create a new node
         if (node == null) {
           node.children[letter] = new Node();
           insert(node.children[letter], sequence, i + 1);
